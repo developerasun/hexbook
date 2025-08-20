9be132e67e3c17e53e030a81ca0af818160454eb
developerasun <nellow1102@gmail.com>
2025-08-20 19:21:25 +0900

    feat: init simple api with gin

---
+++ server/src/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	env "github.com/joho/godotenv"
+)
+
+const (
+	ROOT = ""
+	PORT = 3010
+)
+
+func main() {
+	router := gin.Default()
+	router.SetTrustedProxies(nil)
+	hasError := env.Load("../.run.env")
+
+	if hasError != nil {
+		log.Fatalln("main.go: can't load secrets correctly", hasError.Error())
+		return
+	}
+
+	root := router.Group(ROOT)
+
+	root.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNoContent, gin.H{})
+	})
+
+	root.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	router.Run(":" + os.Getenv("PORT"))
+}
