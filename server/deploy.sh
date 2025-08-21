018f0fbf40126fb2d87afb5edb5d91bea56d622d
developerasun <nellow1102@gmail.com>
2025-08-21 14:45:32 +0900

    feat: configure go setup and cicd script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,3 +1,4 @@
-docker service rm fatcat_backend
+# docker build -t fatcat-backend:latest .
+docker service rm fatcat_backend fatcat_tunnel
 docker stack up -c ./service.yaml fatcat
 docker service ls
