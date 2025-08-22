b50b6bdb984682884db2ba6b4f0d1bb2d9cf98f2
developerasun <nellow1102@gmail.com>
2025-08-22 20:04:21 +0900

    refactor: reset svelte kit

--- client/src/routes/+page.ts
+++
@@ -1,6 +0,0 @@
-import type { PageLoad } from './$types';
-export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('http://localhost:3010/health');
-	const data = await response.json();
-	return { message: data };
-};
