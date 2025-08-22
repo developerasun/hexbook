741b1210cc4b637dc51467e15784208ae09d536e
developerasun <nellow1102@gmail.com>
2025-08-22 20:30:34 +0900

    Revert "refactor: reset svelte kit"
    This reverts commit b50b6bdb984682884db2ba6b4f0d1bb2d9cf98f2.

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import type { PageProps } from './$types';
+	let count = $state(0);
+	$effect(() => {
+		console.log('mounted');
+		return () => console.info('cleaned');
+	});
+	const onCount = () => count++;
+	let { data }: PageProps = $props();
+</script>
+
+<h1>Welcome to SvelteKitss</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>{data.message.message}</p>
+<!--  -->
+
+<Button class="my-4 border-2 border-blue-400" onclick={onCount}>Click me\</Button>
+
+<p>count: {count}</p>
