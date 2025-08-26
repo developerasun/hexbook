e535e3e469ad331ecdc62aa9150e78022cdc2a1c
developerasun <nellow1102@gmail.com>
2025-08-26 18:12:36 +0900

    refactor: rebuild docker image based on new dir

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,4 +1,4 @@
-# docker build -t fatcat-backend:latest .
+docker build -t fatcat-backend:latest .
 docker service rm fatcat_backend fatcat_tunnel
 docker stack up -c ./service.yaml fatcat
 docker service ls
