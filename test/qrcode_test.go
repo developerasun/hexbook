fd14067e47a246ccdf0eaef9bc9082b8acfd0ff8
developerasun <nellow1102@gmail.com>
2025-10-08 16:31:28 +0900

    test: add address prefix test case

--- test/qrcode_test.go
+++ test/qrcode_test.go
@@ -15,6 +15,7 @@ import (
 )
 
 func TestCreateQrCodeAtPath(t *testing.T) {
+	t.Skip()
 	png, err := qrcode.Encode("https://example.org", qrcode.Medium, 256)
 
 	if err != nil {
@@ -35,10 +36,23 @@ func TestCreateQrCodeAtPath(t *testing.T) {
 	t.Log("root: ", root)
 	t.Log("filename: ", filename)
 
+	// @dev path might differ based on where executable runs
 	targetPath := strings.Join([]string{root, "assets", "qrcode", filename}, "/")
 	wErr := os.WriteFile(targetPath, png, constant.FilePermUserReadWriteGroupRead)
 	if wErr != nil {
 		log.Println(wErr.Error())
 		t.FailNow()
 	}
+}
+
+func TestAddressPrefix(t *testing.T) {
+	address := "0x5a27fdA4A09B3feF34c5410de1c5F3497B8EBa11"
+	after, found := strings.CutPrefix(address, "0x")
+
+	if !found {
+		t.Log("can't find eth address prefix: ", address)
+		t.FailNow()
+	}
+
+	log.Println("sliced: ", after)
 }
