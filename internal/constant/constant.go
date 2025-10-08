1af96ae90892e6ac17831eab057790b9fa2b1564
developerasun <nellow1102@gmail.com>
2025-10-08 14:34:51 +0900

    build: update constants

--- internal/constant/constant.go
+++ internal/constant/constant.go
@@ -1,8 +1,11 @@
 package constant
 
 const (
-	MAX_SOCKET_CLIENT = 500
-	ROUTE_ROOT        = "/"
-	ROUTE_API         = "/api"
-	ROUTE_VIEW        = "/view"
+	MAX_SOCKET_CLIENT              = 500
+	ROUTE_ROOT                     = "/"
+	ROUTE_API                      = "/api"
+	ROUTE_VIEW                     = "/view"
+	FilePermUserReadWrite          = 0600
+	FilePermUserReadWriteGroupRead = 0644
+	FilePermExecutable             = 0755
 )
