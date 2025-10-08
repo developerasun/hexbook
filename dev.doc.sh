b6f34929d13cde173c287080fe1d25cc715c7728
developerasun <nellow1102@gmail.com>
2025-10-08 21:23:45 +0900

    refactor: simplify path

--- dev.doc.sh
+++ dev.doc.sh
@@ -1,2 +1,2 @@
 # generate swagger doc, should run at root to include all deps
-swag init -g cmd/main.go -o /home/asun/project/hexbook/docs --parseDependency --parseInternal
+swag init -g cmd/main.go -o ./docs --parseDependency --parseInternal
