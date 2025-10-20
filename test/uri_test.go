95d9fc0891c87eecdf3fff1036512810e897ddd2
developerasun <nellow1102@gmail.com>
2025-10-21 00:09:58 +0900

    test: add test case for mm deeplink

---
+++ test/uri_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	hook "github.com/hexbook/pkg"
+)
+
+func TestQRCodeURI(t *testing.T) {
+	assert := assert.New(t)
+	supported := "metamask"
+	unsupported := "binance"
+
+	assert.Panics(func() {
+		hook.BuildBaseUrlByWallet(unsupported)
+	})
+
+	var baseUrl string
+	assert.NotPanics(func() {
+		baseUrl = hook.BuildBaseUrlByWallet(supported)
+	})
+
+	t.Log("baseUrl: ", baseUrl)
+}
+
+func TestMetamaskDeeplink(t *testing.T) {
+	assert := assert.New(t)
+	generated := "https://metamask.app.link/send/pay-0x5a27fdA4A09B3feF34c5410de1c5F3497B8EBa11@1?value=1e15"
+	qd := hook.QRCodeData{
+		AppType:   "metamask",
+		Wallet:    "0x5a27fdA4A09B3feF34c5410de1c5F3497B8EBa11",
+		ChainId:   1,
+		Amount:    "1e15",
+		TokenType: "eth",
+		Decimal:   18,
+	}
+	// eip681 scheme
+	options := hook.UriOption{Prefix: "pay"}
+	deeplink := hook.BuildMetamaskDeeplink(qd, &options)
+
+	assert.Equal(generated, deeplink)
+	t.Log(deeplink)
+}
