ee940ffa601309da099ee9c2da4a58e2a6492687
developerasun <nellow1102@gmail.com>
2025-10-23 21:22:54 +0900

    refactor: minor updates

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t hexbook-backend:latest .
-docker service rm hexbook_backend hexbook_servertunnel
+docker service rm hexbook_backend
 docker stack up -c ./service.yaml hexbook
 docker service ls
