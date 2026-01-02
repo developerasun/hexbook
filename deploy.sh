9445d810fa8417043749c0b6fe5a668bae0ed589
developerasun <nellow1102@gmail.com>
2026-01-02 17:31:05 +0900

    refactor: redeploy and archive

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-docker build -t hexbook-backend:latest .
-docker service rm hexbook_backend
-docker stack up -c ./service.yaml hexbook
+docker build -t hub-hexbook:latest .
+docker service rm hub_hexbook
+docker stack up -c ./service.yaml hub
+echo [LOG] waiting 5 seconds to deploy
+sleep 5
+echo [LOG] done, listing up services
 docker service ls
